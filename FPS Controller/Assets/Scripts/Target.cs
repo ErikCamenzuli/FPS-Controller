40afb06d1f0f8d8b881d5edd0024a07ecf6e52b5
Erik <erik.camenzuli@hotmail.com>
2019-10-28 23:51:35 +1100

    Added gun

---
+++ FPS Controller/Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float hp = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        hp -= amount;
+        if (hp <= 0f)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+}
