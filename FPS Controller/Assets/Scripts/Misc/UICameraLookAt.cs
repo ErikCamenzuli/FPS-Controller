8e251a0f965ec68035bfe74afce4afe8db01580e
Stacks <erik.camenzuli@hotmail.com>
2019-11-28 10:16:05 +1100

    fixed yesterdays autism
    added:
    + Sound
    + Particles

---
+++ FPS Controller/Assets/Scripts/Misc/UICameraLookAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICameraLookAt : MonoBehaviour
+{
+    public Camera lookAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = lookAt.transform.position - transform.position;
+        v.x = v.z = 0f;
+        transform.LookAt(lookAt.transform.position - v);
+        transform.rotation = (lookAt.transform.rotation);
+    }
+}
